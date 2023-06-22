f392ad4b2abe182396a1902a80812d28a3f08281
vananh2110211 <vananh2110211@gmail.com>
2023-06-22 14:15:58 +0700

    fix

--- lib/pages/page_detail.dart
+++ lib/pages/page_detail.dart
@@ -16,18 +16,18 @@ class page_detail extends StatelessWidget {
         backgroundColor: Colors.white,
         actions:<Widget> [
             IconButton(
-              icon: Icon(Icons.arrow_back_ios, size: 20, color: Colors.black),
+              icon: Icon(Icons.arrow_back_ios, size: 32, color: Colors.black),
               onPressed: () {
                 Navigator.pop(context);
               },
             ),
             Spacer(),
             IconButton(
-              icon: Icon(Icons.shopping_cart, size: 20, color: Colors.black),
+              icon: Icon(Icons.shopping_cart, size: 32, color: Colors.black),
               onPressed: () {},
             ),
             PopupMenuButton(
-              icon: Icon(Icons.more_vert, size: 20, color: Colors.black),
+              icon: Icon(Icons.more_vert, size: 32, color: Colors.black),
               itemBuilder: (context) => [
                 PopupMenuItem(
                   value: 1,
@@ -116,6 +116,7 @@ class page_detail extends StatelessWidget {
             fit: BoxFit.cover,
           ),
           SizedBox(height: 10),
+          p_options(),
           p_detail(),
           p_deals(),
           p_seller(),
@@ -124,6 +125,79 @@ class page_detail extends StatelessWidget {
         ],
       ),
       bottomNavigationBar: p_bottombar(),
+    );
+  }
+
+  Container p_options() {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.centerLeft,
+            margin: EdgeInsets.only(left: 10, bottom: 10),
+            child: 
+            Text(
+              "Phân loại sản phẩm",
+              style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+            )),
+          Row(
+            children: [
+              Expanded(
+                child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                padding: const EdgeInsets.all(0.5),
+                child:
+                Row(
+                children: [
+                  Image.network(
+                    product['image'],
+                    height: 100,
+                    width: 100,
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(width: 12),
+                  Image.network(
+                    product['image'],
+                    height: 100,
+                    width: 100,
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(width: 12),
+                  Image.network(
+                    product['image'],
+                    height: 100,
+                    width: 100,
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(width: 12),
+                  Image.network(
+                    product['image'],
+                    height: 100,
+                    width: 100,
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(width: 12),
+                  Image.network(
+                    product['image'],
+                    height: 100,
+                    width: 100,
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(width: 12),
+                 ],
+                )
+                          ),
+              ),
+            SizedBox(
+              width: 30,
+              child: Icon(Icons.arrow_forward_ios, size: 28)),
+            ]
+          ),
+        ],
+      ),
     );
   }
 
@@ -247,7 +321,11 @@ class page_detail extends StatelessWidget {
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           Text(
-                            'NewPrice',
+                            '\$'+ product['price'],
+                            style: TextStyle(
+                            fontSize: 22,
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold)
                           ),
                           SizedBox(width: 15),
                           Text(
@@ -370,7 +448,7 @@ class page_detail extends StatelessWidget {
               trimMode: TrimMode.Line,
               trimCollapsedText: 'Show more',
               trimExpandedText: 'Show less',
-              moreStyle: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+              moreStyle: TextStyle(fontSize: 13, fontWeight: FontWeight.bold, color: Colors.grey),
             ),
             Icon(Icons.keyboard_arrow_down, size: 28),
           ],
@@ -484,7 +562,7 @@ class page_detail extends StatelessWidget {
 
   Container p_bottombar() {
     return Container(
-      height: 65,
+      height: 50,
       margin: const EdgeInsets.symmetric(vertical: 1, horizontal: 5),
       decoration: BoxDecoration(
           color: Color.fromARGB(255, 238, 238, 238),
@@ -502,12 +580,12 @@ class page_detail extends StatelessWidget {
           ElevatedButton(
             onPressed: () => {},
             style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(Colors.lightBlue),
+              backgroundColor: MaterialStateProperty.all(const Color.fromARGB(255, 120, 207, 247)),
             ),
             child: Center(
               child: Icon(
                 Icons.share,
-                color: Colors.white,
+                color: Colors.black,
                 size: 32,
               ),
             ),
@@ -515,12 +593,12 @@ class page_detail extends StatelessWidget {
           ElevatedButton(
             onPressed: () => {},
             style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(Colors.blue),
+              backgroundColor: MaterialStateProperty.all(const Color.fromARGB(255, 69, 166, 245)),
             ),
             child: Center(
               child: Icon(
-                Icons.shopping_cart,
-                color: Colors.white,
+                Icons.add_shopping_cart,
+                color: Colors.black,
                 size: 32,
               ),
             ),
@@ -528,7 +606,7 @@ class page_detail extends StatelessWidget {
           ElevatedButton(
             onPressed: () => {},
             style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(Colors.blue),
+              backgroundColor: MaterialStateProperty.all(const Color.fromARGB(255, 21, 146, 249)),
             ),
             child: Center(
               child: Text("Mua ngay",
